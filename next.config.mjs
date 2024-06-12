0f814a2d3a5f41d0c58b5d870483aa7501e12652
Niilo Saarlemo <45739791+saarlemo@users.noreply.github.com>
2024-06-12 19:43:34 +0300

    CSS:n latauksen korjaus

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     skipTrailingSlashRedirect: true,
-    assetPrefix: './',
+    assetPrefix: isProd ? 'https://saarlemo.fi/' : "./",
     basePath: '',
     images: { unoptimized: true }
 };
